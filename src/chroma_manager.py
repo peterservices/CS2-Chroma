2b24b87d432e50abc4e93f244b83b27d74b8b8f5
Ticataco <148506232+peterservices@users.noreply.github.com>
2026-09-25 21:21:13 -0600

    Add full support for effects on devices other than keyboards Currently mice and headsets

--- src/chroma_manager.py
+++ src/chroma_manager.py
@@ -93,95 +93,119 @@ class ChromaControl(websocket.WebSocket):
         Update the keyboard's color with active effects, and update any effect animations.
         """
         while True:
-            expiring_effects = []
-            effect_changed = False
-            with self.chroma_state.lock:
-                for effect in self.chroma_state.keyboard_effects:
-                    if effect.update_rate is not None and time.time() - effect.last_update >= effect.update_rate:
-                        if effect.expires_after_updates is not None:
-                            if effect.expires_after_updates == 0:
-                                expiring_effects.append(effect)
-                                continue
-                            effect.expires_after_updates -= 1
+            for device, enabled in self.config.devices:
+                if not enabled:
+                    continue
+                device = device.upper()
 
-                        if effect.decay_amount is not None:
-                            effect_changed = True
-                            max_value = 0.0
-                            for _, row_v in enumerate(effect.colors):
-                                for column, column_v in enumerate(row_v):
-                                    color: list[float] = []
-                                    for i in range(3):
-                                        color.append((column_v[i] - effect.decay_amount) if column_v[i] >= effect.decay_amount else 0)
-                                        max_value = max(max_value, color[-1])
-                                    row_v[column] = tuple(color)
-                            if effect.expires_after_updates is None and max_value == 0.0:
-                                expiring_effects.append(effect)
-                                continue
+                effects = self.chroma_state.get_effects_list_from_device_type(device)
+                match device:
+                    case "KEYBOARD":
+                        device_color_columns = 24
+                        device_color_rows = 8
+                    case _:
+                        device_color_columns = 1
+                        device_color_rows = 1
 
-                        match effect.type:
-                            case "WAVE":
+                expiring_effects = []
+                effect_changed = False
+                with self.chroma_state.lock:
+                    for effect in effects:
+                        if effect.update_rate is not None and time.time() - effect.last_update >= effect.update_rate:
+                            if effect.expires_after_updates is not None:
+                                if effect.expires_after_updates == 0:
+                                    expiring_effects.append(effect)
+                                    continue
+                                effect.expires_after_updates -= 1
+
+                            if effect.decay_amount is not None:
                                 effect_changed = True
-                                update_wave_effect(effect)
-                            case "EXPLOSION":
-                                effect_changed = True
-                                update_explosion_effect(effect)
-                        effect.last_update = time.time()
-                for effect in expiring_effects:
-                    self.chroma_state.keyboard_effects.remove(effect)
+                                max_value = 0.0
+                                for _, row_v in enumerate(effect.colors):
+                                    for column, column_v in enumerate(row_v):
+                                        color: list[float] = []
+                                        for i in range(3):
+                                            color.append((column_v[i] - effect.decay_amount) if column_v[i] >= effect.decay_amount else 0)
+                                            max_value = max(max_value, color[-1])
+                                        row_v[column] = tuple(color)
+                                if effect.expires_after_updates is None and max_value == 0.0:
+                                    expiring_effects.append(effect)
+                                    continue
 
-                if not effect_changed:
-                    effect_changed = self.chroma_state.keyboard_effects != self.chroma_state.previous_keyboard_effects
-                if effect_changed:
-                    self.chroma_state.previous_keyboard_effects = deepcopy(self.chroma_state.keyboard_effects)
+                            match effect.type:
+                                case "WAVE":
+                                    effect_changed = True
+                                    update_wave_effect(effect)
+                                case "EXPLOSION":
+                                    effect_changed = True
+                                    update_explosion_effect(effect)
+                            effect.last_update = time.time()
+                    for effect in expiring_effects:
+                        self.chroma_state.remove_effect(effect)
 
-                if effect_changed and len(self.chroma_state.keyboard_effects) > 0:
-                    colors = [[(0.0, 0.0, 0.0) for _ in range(24)] for _ in range(8)]
-                    for effect in self.chroma_state.keyboard_effects:
-                        match effect.method:
-                            case "ADD":
-                                # Add everything
-                                for row, row_v in enumerate(effect.colors):
-                                    for column, column_v in enumerate(row_v):
-                                        colors[row][column] = tuple([min(colors[row][column][i] + column_v[i], 1.0) for i in range(3)])
-                            case "FILL":
-                                # Fill everything
-                                colors = deepcopy(effect.colors)
-                            case "FILL_EMPTY":
-                                # Fill only remaining zero values
-                                for row, row_v in enumerate(effect.colors):
-                                    for column, column_v in enumerate(row_v):
-                                        if colors[row][column] == (0.0, 0.0, 0.0):
-                                            colors[row][column] = column_v
-                            case "FILL_NO_ZERO":
-                                # Fill everything, but don't fill with zero values
-                                for row, row_v in enumerate(effect.colors):
-                                    for column, column_v in enumerate(row_v):
-                                        if column_v != (0.0, 0.0, 0.0):
-                                            colors[row][column] = column_v
-                            case "MULTIPLY":
-                                # Multiply everything
-                                for row, row_v in enumerate(effect.colors):
-                                    for column, column_v in enumerate(row_v):
-                                        colors[row][column] = tuple([min(colors[row][column][i] * column_v[i], 1.0) for i in range(3)])
-                    # Convert the float colors to decimal colors usable by the Chroma SDK API
-                    for row, row_v in enumerate(colors):
-                        for column, column_v in enumerate(row_v):
-                            colors[row][column] = float_to_decimal(column_v)
+                    if not effect_changed:
+                        effect_changed = effects != self.chroma_state.get_previous_effects(device)
+                    if effect_changed:
+                        self.chroma_state.save_previous_effects(device)
 
-                    if self.chroma_connected_event.is_set(): # Only send data if we are connected
-                        self.chroma_send({
-                            "endpoint": "keyboard",
-                            "effect": "CHROMA_CUSTOM2",
-                            "param": {
-                               "color": colors,
-                               "key": [[0 for _ in range(22)] for _ in range(6)] # Make key param all zeros because it's not needed
-                            }
-                        })
-                elif effect_changed:
-                    if self.chroma_connected_event.is_set(): # Only send data if we are connected
-                        self.chroma_send({
-                            "endpoint": "keyboard",
-                            "effect": "CHROMA_NONE"
-                        })
+                    if effect_changed and len(effects) > 0:
+                        colors = [[(0.0, 0.0, 0.0) for _ in range(device_color_columns)] for _ in range(device_color_rows)]
+                        for effect in effects:
+                            match effect.method:
+                                case "ADD":
+                                    # Add everything
+                                    for row, row_v in enumerate(effect.colors):
+                                        for column, column_v in enumerate(row_v):
+                                            colors[row][column] = tuple([min(colors[row][column][i] + column_v[i], 1.0) for i in range(3)])
+                                case "FILL":
+                                    # Fill everything
+                                    colors = deepcopy(effect.colors)
+                                case "FILL_EMPTY":
+                                    # Fill only remaining zero values
+                                    for row, row_v in enumerate(effect.colors):
+                                        for column, column_v in enumerate(row_v):
+                                            if colors[row][column] == (0.0, 0.0, 0.0):
+                                                colors[row][column] = column_v
+                                case "FILL_NO_ZERO":
+                                    # Fill everything, but don't fill with zero values
+                                    for row, row_v in enumerate(effect.colors):
+                                        for column, column_v in enumerate(row_v):
+                                            if column_v != (0.0, 0.0, 0.0):
+                                                colors[row][column] = column_v
+                                case "MULTIPLY":
+                                    # Multiply everything
+                                    for row, row_v in enumerate(effect.colors):
+                                        for column, column_v in enumerate(row_v):
+                                            colors[row][column] = tuple([min(colors[row][column][i] * column_v[i], 1.0) for i in range(3)])
+                        # Convert the float colors to decimal colors usable by the Chroma SDK API
+                        for row, row_v in enumerate(colors):
+                            for column, column_v in enumerate(row_v):
+                                colors[row][column] = float_to_decimal(column_v)
+
+                        if self.chroma_connected_event.is_set(): # Only send data if we are connected
+                            match device:
+                                case "KEYBOARD":
+                                    self.chroma_send({
+                                        "endpoint": "keyboard",
+                                        "effect": "CHROMA_CUSTOM2",
+                                        "param": {
+                                           "color": colors,
+                                           "key": [[0 for _ in range(22)] for _ in range(6)] # Make key param all zeros because it's not needed
+                                        }
+                                    })
+                                case _:
+                                    self.chroma_send({
+                                        "endpoint": device.lower(),
+                                        "effect": "CHROMA_STATIC",
+                                        "param": {
+                                            "color": colors[0][0]
+                                        }
+                                    })
+                    elif effect_changed:
+                        if self.chroma_connected_event.is_set(): # Only send data if we are connected
+                            self.chroma_send({
+                                "endpoint": device.lower(),
+                                "effect": "CHROMA_NONE"
+                            })
 
 # By @peterservices
